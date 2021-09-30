7ca4c07678f496ea621bf3f69f91a8cf41b2041e
Rhalim Calnetic <rhalim@calnetic.com>
2021-09-30 10:23:35 +0700

    darkmode datastore

--- app/src/main/java/com/example/submission2/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/submission2/ui/main/MainActivity.kt
@@ -5,29 +5,38 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SearchView
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.submission2.ui.detailUser.DetailUserActivity
 import com.example.submission2.R
 import com.example.submission2.adapter.UserGithubAdapter
-import com.example.submission2.database.Favorite
 import com.example.submission2.databinding.ActivityMainBinding
-import com.example.submission2.helper.DateHelper
+import com.example.submission2.helper.SettingModelFactory
+import com.example.submission2.helper.SettingPreferences
 import com.example.submission2.model.User
 import com.example.submission2.model.UserResponse
 import com.example.submission2.ui.favorite.FavoriteActivity
 import com.example.submission2.ui.main.MainViewModel.Companion.DEFAULT_USERNAME
 
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val mainViewModel by viewModels<MainViewModel>()
+
+    private lateinit var settingViewModel: SettingViewModel
+    private var themeDark : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,16 +49,29 @@ class MainActivity : AppCompatActivity() {
         mainViewModel.isLoading.observe(this, {
             showLoading(it)
         })
-
         mainViewModel.isError.observe(this, {
             Toast.makeText(this, "Gagal Memuat data !", Toast.LENGTH_LONG).show()
         })
         binding.rvUsers.setHasFixedSize(true)
 
+        val pref = SettingPreferences.getInstance(dataStore)
+        settingViewModel = ViewModelProvider(this, SettingModelFactory(pref)).get(
+            SettingViewModel::class.java
+        )
+        settingViewModel.getThemeSettings().observe(this,
+            { isDarkModeActive: Boolean ->
+                if (isDarkModeActive) {
+                    themeDark = true
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                } else {
+                    themeDark = false
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+            })
+
     }
 
     private fun setUserGithub(user: UserResponse) {
-
         if (user.items.isEmpty()) {
             binding.resultKosong.visibility = View.VISIBLE
             Toast.makeText(this, "Data tidak ditemukan !", Toast.LENGTH_SHORT).show()
@@ -86,6 +108,13 @@ class MainActivity : AppCompatActivity() {
         val inflater = menuInflater
         inflater.inflate(R.menu.option_menu, menu)
 
+        val titleMode = menu.findItem(R.id.menu2)
+        if(themeDark) {
+            titleMode.setTitle(R.string.normal_mode)
+        } else {
+            titleMode.setTitle(R.string.dark_mode)
+        }
+
         val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
         val searchView = menu.findItem(R.id.search).actionView as SearchView
 
@@ -114,8 +143,11 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             R.id.menu2 -> {
-                val favoriteIntent = Intent(this@MainActivity, FavoriteActivity::class.java)
-                startActivity(favoriteIntent)
+                if(themeDark) {
+                    settingViewModel.saveThemeSetting(false)
+                } else {
+                    settingViewModel.saveThemeSetting(true)
+                }
                 true
             }
             R.id.favorite -> {
